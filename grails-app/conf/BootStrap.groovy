6fbf2e10143d21feb90ca5f7708726a1c83d40e1
Igor Shkliar <igorsh@default-value.com>
2016-04-04 22:51:32 +0300

    Configuration. Add simple BootStrap data

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,28 @@
+import ua.ck.solo.aidkit.Role
+import ua.ck.solo.aidkit.User
+import ua.ck.solo.aidkit.UserRole
+
 class BootStrap {
 
     def init = { servletContext ->
+        if (!User.count()) createSampleData()
     }
     def destroy = {
+    }
+
+    private void createSampleData() {
+
+        def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
+        def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
+
+        def testUser = new User(username: 'admin', password: 'admin')
+        testUser.save(flush: true)
+
+        UserRole.create testUser, adminRole, true
+
+        assert User.count() == 1
+        assert Role.count() == 2
+        assert UserRole.count() == 1
+
     }
 }
