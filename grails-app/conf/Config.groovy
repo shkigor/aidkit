0195d9db782e5b31870312a93d2a42f1f69fbeb3
Igor Shkliar <igorsh@default-value.com>
2016-04-04 22:46:27 +0300

    Spring Security. Run command s2-quickstart ua.ck.solo.aidkit User Role

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -115,3 +115,20 @@ log4j.main = {
            'org.hibernate',
            'net.sf.ehcache.hibernate'
 }
+
+
+// Added by the Spring Security Core plugin:
+grails.plugin.springsecurity.userLookup.userDomainClassName = 'ua.ck.solo.aidkit.User'
+grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'ua.ck.solo.aidkit.UserRole'
+grails.plugin.springsecurity.authority.className = 'ua.ck.solo.aidkit.Role'
+grails.plugin.springsecurity.controllerAnnotations.staticRules = [
+	'/':                ['permitAll'],
+	'/index':           ['permitAll'],
+	'/index.gsp':       ['permitAll'],
+	'/assets/**':       ['permitAll'],
+	'/**/js/**':        ['permitAll'],
+	'/**/css/**':       ['permitAll'],
+	'/**/images/**':    ['permitAll'],
+	'/**/favicon.ico':  ['permitAll']
+]
+
