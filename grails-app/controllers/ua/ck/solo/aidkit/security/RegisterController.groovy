05cdd840758a1669e3c40375e794789bf26bcb7f
Igor Shkliar <igorsh@default-value.com>
2016-04-05 21:11:58 +0300

    Controllers. Spring Security. Override register action. Set username=email

--- grails-app/controllers/ua/ck/solo/aidkit/security/RegisterController.groovy
+++ grails-app/controllers/ua/ck/solo/aidkit/security/RegisterController.groovy
@@ -1,7 +1,14 @@
 package ua.ck.solo.aidkit.security
 
 import grails.plugin.springsecurity.annotation.Secured
+import grails.plugin.springsecurity.ui.RegisterCommand
 
 @Secured(['permitAll'])
 class RegisterController extends grails.plugin.springsecurity.ui.RegisterController {
+
+    @Override
+    def register(RegisterCommand registerCommand) {
+        registerCommand.username = registerCommand.email
+        super.register(registerCommand)
+    }
 }
