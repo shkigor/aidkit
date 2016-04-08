06556c9082bef8cec9c47678dfa0e1596a781d9a
Igor Shkliar <igorsh@default-value.com>
2016-04-09 00:01:07 +0300

    Controller. Spring Security. Add 'ROLE_USER' access to DrugController

--- grails-app/controllers/ua/ck/solo/aidkit/DrugController.groovy
+++ grails-app/controllers/ua/ck/solo/aidkit/DrugController.groovy
@@ -2,7 +2,7 @@ package ua.ck.solo.aidkit
 
 import grails.plugin.springsecurity.annotation.Secured
 
-@Secured(['ROLE_ADMIN'])
+@Secured(['ROLE_ADMIN', 'ROLE_USER'])
 class DrugController {
     static scaffold = true
 }
