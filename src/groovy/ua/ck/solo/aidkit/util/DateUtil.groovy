0a66e0807b014126fdf7ae7c23a57eac2a8b7070
Igor Shkliar <igorsh@default-value.com>
2016-04-09 14:54:16 +0300

    Groovy. Util. Add DateUtil class

---
+++ src/groovy/ua/ck/solo/aidkit/util/DateUtil.groovy
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2012-2015. Quad Nova, Inc. All rights reserved.
+ */
+
+package ua.ck.solo.aidkit.util
+
+class DateUtil {
+	static final int MS_IN_ONE_MINUTE = 60000
+
+	/**
+	 * Date format constant for RU with date part. E.g. 11.12.1978
+	 */
+	static final String DATE_FORMAT_RU_DATE = 'dd.MM.yyyy'
+	static final String DATE_FORMAT_RU_DATE_SHORT = 'dd.MM.yy'
+
+	/**
+	 * Date format constant for US with time part. E.g. 07:15 PM.
+	 */
+	static final String DATE_FORMAT_RU_TIME = 'HH:mm'
+}
+
